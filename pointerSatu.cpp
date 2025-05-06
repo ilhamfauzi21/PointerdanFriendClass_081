61186e2ac3e21a98be9511cc09833dd551d22d92
ilhamfauzi21 <ilham.fauzi.ft21@mail.umy.ac.id>
2025-05-06 15:41:30 +0700

    input data mahasiswa

--- pointerSatu.cpp
+++ pointerSatu.cpp
@@ -17,3 +17,18 @@ public:
         cout << "No Induk : " << nim << endl;
     }
 };
+int main()
+{
+    mahasiswa mhs(1); // objek dibuat dengan nim 1
+    mhs.showNim();
+
+    mahasiswa &refMhs = mhs; // reference ke objek mhs
+    refMhs.nim = 2;          // ubah nilai nim melalui reference
+    mhs.showNim();
+
+    mahasiswa *pMhs = &mhs;  // pointer ke objek mhs
+    pMhs->nim = 3;           // ubah nilai nim melalui pointer
+    pMhs->showNim();
+
+    return 0;
+}

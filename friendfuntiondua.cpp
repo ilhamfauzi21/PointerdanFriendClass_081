1cbee12fc3d882c44736059488aab364e7aa7096
ilhamfauzi21 <ilham.fauzi.ft21@mail.umy.ac.id>
2025-05-06 17:22:09 +0700

    membuat public nilai

--- friendfuntiondua.cpp
+++ friendfuntiondua.cpp
@@ -8,3 +8,12 @@ class manusia
 public:
     void showNilaiPelajar(pelajar &a);
 };
+class pelajar
+{
+private:
+    int nilai;
+
+public:
+    pelajar() { nilai = 100; }
+    friend void manusia::showNilaiPelajar(pelajar &a);
+};
